41f304526c4810d02c11a06cff78a5ef7fab3f3d
Anezz12 <harsenaargrtya1@gmail.com>
2024-04-17 01:43:25 +0700

    update

--- components/layout/RightSideBar.jsx
+++ components/layout/RightSideBar.jsx
@@ -6,13 +6,41 @@ const RightSideBar = () => {
     <div className="sticky right-0 top-0 z-20 h-screen w-[300px] xl:w-[350px] flex flex-col gap-12 overflow-auto pl-6 pr-10 py-6 max-lg:hidden">
       <div className="flex flex-col gap-4">
         <h3 className="text-light-1 text-heading3-bold">Sponsored</h3>
-        <Image
-          src="/assets/ad.jpg"
-          alt="ad"
-          width={280}
-          height={200}
-          className="rounded-lg"
-        />
+        <div className="relative overflow-hidden rounded-lg">
+          <div className="flex animate-slider">
+            {/* Last slide duplicated */}
+            <div className="w-full shrink-0">
+              <Image
+                src="/assets/ad2.jpg"
+                alt="ad"
+                width={280}
+                height={200}
+                className="rounded-lg"
+              />
+            </div>
+
+            {/* Original slides */}
+            <div className="w-full shrink-0">
+              <Image
+                src="/assets/ad.jpg"
+                alt="ad"
+                width={280}
+                height={200}
+                className="rounded-lg"
+              />
+            </div>
+            {/* First slide duplicated */}
+            <div className="w-full shrink-0">
+              <Image
+                src="/assets/ad2.jpg"
+                alt="ad"
+                width={280}
+                height={200}
+                className="rounded-lg"
+              />
+            </div>
+          </div>
+        </div>
         <p className="text-body-bold text-light-1">Febreze Air Freshener</p>
         <p className="text-small-semibold text-light-2">
           Instant odor fighting and a burst of freshness. Amazing summer scent.
